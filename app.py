412258849ba4df69b44093235e93b3826e749f04
Filip H <107533273+hugmonke@users.noreply.github.com>
2025-06-08 14:21:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import flask
 import fastai.vision.all as fa
 import os
+from PIL import ImageFile
+import wikipedia
+import wikipedia.exceptions
 
 app = flask.Flask(__name__)
 UPLOAD_FOLDER = 'static/uploaded_images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def get_wikipedia_summary(genus_name):
+    try:
+        wikipedia.set_lang("en")
+        page = wikipedia.page(genus_name, auto_suggest=False)
+        return page.summary.split('\n')[0]
+    except wikipedia.exceptions.DisambiguationError as e:
+        try:
+            return wikipedia.summary(e.options[0], auto_suggest=False).split('\n')[0]
+        except:
+            return "Disambiguation - Could not retrieve description :("
+    except wikipedia.exceptions.PageError:
+        return "No description available :/"
+    except:
+        return "Error fetching description >:("
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
     if flask.request.method == 'POST':
-        
         if 'mushroom' not in flask.request.files:
             return flask.redirect(flask.request.url)
         
@@ -24,18 +40,22 @@ def home():
             return flask.redirect(flask.request.url)
         
         if file:
+            ImageFile.LOAD_TRUNCATED_IMAGES = True
             imagepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(imagepath)
 
             predicted_name, prediction_idx, probabilities = model.predict(imagepath)
             confidence = 100*float(probabilities[prediction_idx])
             
+            wiki_description = get_wikipedia_summary(genus_name=predicted_name)
+            
             return flask.render_template(
-                                    'index.html', 
-                                    prediction=predicted_name, 
-                                    confidence=confidence,
-                                    image_path=file.filename
-                                    )
+                'index.html', 
+                prediction=predicted_name, 
+                confidence=confidence,
+                image_path=file.filename,
+                description=wiki_description
+            )
     
     return flask.render_template('index.html')
 
